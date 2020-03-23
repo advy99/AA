8b886f89251b7e19c64fbfa9d26b7667062271b1
Antonio David Villegas <advy99@gmail.com>
2020-03-23 18:19:52 +0100

    acabamos segundo ejercicio apartado uno (creo)

--- practica1/trabajo2.py
+++ practica1/trabajo2.py
@@ -41,12 +41,52 @@ def readData(file_x, file_y):
 
 # Funcion para calcular el error
 def Err(x,y,w):
-    return
+	## Función dada en las diapositivas:
+	#
+	# E_in(w) = 1/N * SUM(w^T * x_n - y_n)^2
+	#
+	# basicamente, hacemos w * x - y al cuadrado, y le hacemos la media
+
+	# https://docs.scipy.org/doc/numpy/reference/generated/numpy.square.html
+	err = np.square(x.dot(w) - y.reshape(-1,1))
+
+	# https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+	err.mean()
+
+	return err
+
+def dErr(x, y, w):
+	# la parte interna sigue igual
+	derivada = x.dot(w) - y.reshape(-1,1)
+
+	# el cuadrado pasa a ser un *2, y restamos una al exponente
+	derivada = 2 * np.mean(x * derivada)
+
+	return derivada
 
 # Gradiente Descendente Estocastico
-def sgd(x, y, tam_batch, iteraciones):
+def sgd(x, y, tasa_aprendizaje, tam_batch, maxIteraciones):
     #
-    return w
+	# diapositiva 17, jussto antes del metodo de newton
+
+	w = np.zeros((3, 1), np.float64)
+
+	iterations = 0
+
+	while iterations < maxIteraciones:
+
+		iterations = iterations + 1
+
+		# https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.choice.html
+		# sacamos tantos indices aleatorios como tam_batch (tamaño del minibatch) queramos
+		# lo sacamos a partir de la forma de x, es decir, x tendrá N filas y 1 columna
+		# pues escogemos de entre las N filas tam_batch
+		indices_minibatch = np.random.choice(x.shape[0], tam_batch, replace=False)
+
+		# aplicamos la funcion dada en la diapositiva 17 del tema 1
+		w = w - tasa_aprendizaje * dErr(x[indices_minibatch], y[indices_minibatch], w)
+
+	return w, iterations
 
 # Pseudoinversa
 def pseudoinverse(matriz_x, vector_y):
@@ -55,18 +95,19 @@ def pseudoinverse(matriz_x, vector_y):
 	# https://docs.scipy.org/doc/numpy/reference/generated/numpy.transpose.html
 	x_traspuesta = matriz_x.transpose()
 
-	# multiplicamos x por su traspuesta https://docs.scipy.org/doc/numpy/reference/generated/numpy.dot.html
-	x_tras_por_x = x_traspuesta.dot(matriz_x)
-
-	# calculamos la inversa: https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.inv.html
-	inversa_x_por_tras = np.linalg.inv(x_tras_por_x)
-
 	# cambiamos de forma Y, no sabemos cuantas filas tendrá, pero tendrá una única columna
 	y_traspuesto = y.reshape(-1, 1)
 
+	# multiplicamos x por su traspuesta https://docs.scipy.org/doc/numpy/reference/generated/numpy.dot.html
+	x_pseudoinversa = x_traspuesta.dot(matriz_x)
+
+	# calculamos la inversa: https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.inv.html
+	x_pseudoinversa = np.linalg.inv(x_pseudoinversa)
+
+	x_pseudoinversa = x_pseudoinversa.dot(x_traspuesta)
 
 	# finalmente calculamos w con la inversa e y
-	w = inversa_x_por_tras.dot(y_traspuesto)
+	w = x_pseudoinversa.dot(y_traspuesto)
 
 	return w
 
@@ -77,7 +118,7 @@ x, y = readData('datos/X_train.npy', 'datos/y_train.npy')
 x_test, y_test = readData('datos/X_test.npy', 'datos/y_test.npy')
 
 
-w = sgd(x, y)
+w, iteraciones = sgd(x, y, 0.1, 64, 100)
 print ('Bondad del resultado para grad. descendente estocastico:\n')
 print ("Ein: ", Err(x,y,w))
 print ("Eout: ", Err(x_test, y_test, w))
