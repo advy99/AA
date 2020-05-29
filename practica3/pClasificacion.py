f16e9b09c3b073c7a900c61165e90dc19a5cd1fb
Antonio David Villegas <advy99@gmail.com>
2020-05-29 16:27:48 +0200

    ya queda poco para el de clasificacion, y el de regresión se hace rápido

--- practica3/pClasificacion.py
+++ practica3/pClasificacion.py
@@ -24,6 +24,9 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import accuracy_score
 
+# para contar tiempos
+import time
+
 np.random.seed(1)
 
 def leer_datos(fichero):
@@ -43,30 +46,36 @@ def leer_datos(fichero):
 
 def evaluar(clasificador, x, y, x_test, y_test, cv):
 	# ajustamos el modelo
-	print("Ajustando el modelo")
+	inicio = time.time();
+	print("\tAjustando el modelo")
 	clasificador.fit(x,y)
-	# aplicamos validación cruzada
-	print("Aplicando 10-k-folds cross-validation")
-	resultado_regresion_lineal = cross_val_score(clasificador, x, y, scoring='neg_mean_squared_error', cv=10)
+	# aplicamos validación cruzada una vez tenemos el modelo ajustado
+	print("\tAplicando", cv, "-k-folds cross-validation")
+	resultado_cross_val = cross_val_score(clasificador, x, y, scoring='accuracy', cv=cv)
+	fin = time.time()
+	print("\tTiempo (en segundos) necesario para ajustar el modelo y aplicar cross-validation: ", fin-inicio)
+	print("\n")
 
 	# predecimos training acorde al modelo
-	print("Prediciendo las etiquetas dentro de training")
+	print("\tPrediciendo las etiquetas dentro de training")
 	y_predecida = clasificador.predict(x).round()
 	# miramos la tasa de aciertos, es decir, cuantos ha clasificado bien
-	print("Obteniendo E_in a partir de la predicción")
+	print("\tObteniendo E_in a partir de la predicción")
 	aciertos = accuracy_score(y, y_predecida)
-	print("Porcentaje de aciertos usando en training", aciertos)
-	print("E_in: ", 1-aciertos)
+	print("\tPorcentaje de aciertos usando en training", aciertos)
+	print("\tE_in: ", 1-aciertos)
 
 
-	print("Prediciendo las etiquetas en test")
+	print("\tPrediciendo las etiquetas en test")
 	# predecimos test acorde al modelo
 	y_predecida_test = clasificador.predict(x_test).round()
 	# miramos la tasa de aciertos, es decir, cuantos ha clasificado bien
-	print("Obteniendo E_test a partir de la predicción")
+	print("\tObteniendo E_test a partir de la predicción")
 	aciertos = accuracy_score(y_test, y_predecida_test)
-	print("Porcentaje de aciertos en test: ", aciertos)
-	print("E_test: ", 1-aciertos)
+	print("\tPorcentaje de aciertos en test: ", aciertos)
+	print("\tE_test: ", 1-aciertos)
+
+	print("\tEvaluación de aciertos usando cross-validation: ", resultado_cross_val.mean())
 
 
 	print("\n\n")
@@ -133,6 +142,15 @@ plt.show()
 input("\n--- Pulsar tecla para continuar ---\n")
 
 
+
+
+
+# evaluaciones de los modelos
+# la primera linea declaramos el objeto de Scikit-Learn para ajustar los parametros
+# y hecho esto llamamos a la función evaluar con el modelo, los datos y
+# el número de k-folds para cross-validation
+
+
 ridgeCsinReg = RidgeClassifier(alpha=0)
 print("Evaluando Ridge Classifier sin regularización:")
 evaluar(ridgeCsinReg, x, y, x_test, y_test, 10)
@@ -151,14 +169,60 @@ print("Evaluando Ridge Classifier con regularización 2:")
 evaluar(ridgeCReg2, x, y, x_test, y_test, 10)
 
 
-logiticReg = LogisticRegression(penalty='l2', solver='newton-cg', C=1)
-print("Evaluando Regresión Logística:")
-evaluar(logiticReg, x, y, x_test, y_test, 10)
 
-logiticReg0 = LogisticRegression(penalty='l2', solver='newton-cg', C=1)
-print("Evaluando Regresión Logística sin regularización:")
-evaluar(logiticReg0, x, y, x_test, y_test, 10)
 
-SGD = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0, learning_rate='constant', eta0=0.01,)
-print("Evaluando SGD Classifier:")
+# logiticReg = LogisticRegression(penalty='l2', solver='newton-cg', C=1)
+# print("Evaluando Regresión Logística:")
+# evaluar(logiticReg, x, y, x_test, y_test, 10)
+#
+# logiticReg0 = LogisticRegression(penalty='l2', solver='newton-cg', C=1)
+# print("Evaluando Regresión Logística sin regularización:")
+# evaluar(logiticReg0, x, y, x_test, y_test, 10)
+#
+
+
+SGD = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0, learning_rate='constant', eta0=0.01, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje constante 0.01:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+
+SGD = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0.0001, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.0001:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+
+SGD = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0.01, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.01:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+SGD = SGDClassifier(loss='squared_loss', penalty='l2', alpha=0.1, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.1:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+SGD = SGDClassifier(loss='squared_loss', penalty='l2', alpha=1, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 1:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+
+
+SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=0, learning_rate='constant', eta0=0.01, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje constante 0.01 y función de perdida hinge:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+
+SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.0001 y función de perdida hinge:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+
+SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=0.01, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.01 y función de perdida hinge:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=0.1, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 0.1 y función de perdida hinge:")
+evaluar(SGD, x, y, x_test, y_test, 10)
+
+SGD = SGDClassifier(loss='hinge', penalty='l2', alpha=1, max_iter=5000)
+print("Evaluando SGD Classifier con tasa de aprendizaje optima (variable) y factor de regularización 1 y función de perdida hinge:")
 evaluar(SGD, x, y, x_test, y_test, 10)
