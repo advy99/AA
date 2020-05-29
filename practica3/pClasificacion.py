c2f8468c388424b92a2682ed21159bb1912cd8c5
Antonio David Villegas <advy99@gmail.com>
2020-05-29 12:19:13 +0200

    continuamos con la práctica, queda menos, el código nos lo hemos quitado rápido

--- practica3/pClasificacion.py
+++ practica3/pClasificacion.py
@@ -41,6 +41,38 @@ def leer_datos(fichero):
 	return x, y
 
 
+def evaluar(clasificador, x, y, x_test, y_test, cv):
+	# ajustamos el modelo
+	print("Ajustando el modelo")
+	clasificador.fit(x,y)
+	# aplicamos validación cruzada
+	print("Aplicando 10-k-folds cross-validation")
+	resultado_regresion_lineal = cross_val_score(clasificador, x, y, scoring='neg_mean_squared_error', cv=10)
+
+	# predecimos training acorde al modelo
+	print("Prediciendo las etiquetas dentro de training")
+	y_predecida = clasificador.predict(x).round()
+	# miramos la tasa de aciertos, es decir, cuantos ha clasificado bien
+	print("Obteniendo E_in a partir de la predicción")
+	aciertos = accuracy_score(y, y_predecida)
+	print("Porcentaje de aciertos usando en training", aciertos)
+	print("E_in: ", 1-aciertos)
+
+
+	print("Prediciendo las etiquetas en test")
+	# predecimos test acorde al modelo
+	y_predecida_test = clasificador.predict(x_test).round()
+	# miramos la tasa de aciertos, es decir, cuantos ha clasificado bien
+	print("Obteniendo E_test a partir de la predicción")
+	aciertos = accuracy_score(y_test, y_predecida_test)
+	print("Porcentaje de aciertos en test: ", aciertos)
+	print("E_test: ", 1-aciertos)
+
+
+	print("\n\n")
+	input("\n--- Pulsar tecla para continuar ---\n")
+	print("\n\n")
+
 
 
 
@@ -51,6 +83,8 @@ x, y = leer_datos(datos_tra)
 print("Leidos ", y.size, " datos con sus respectivas etiquetas")
 
 print("Cada dato del conjunto de datos tiene ", x[0].size, " variables.")
+
+input("\n--- Pulsar tecla para continuar ---\n")
 
 # https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray-in-python
 unique, contador = np.unique(y, return_counts=True)
@@ -67,6 +101,7 @@ plt.xticks(np.arange(10))
 plt.bar(np.arange(10), contador, width=0.4)
 
 plt.show()
+input("\n--- Pulsar tecla para continuar ---\n")
 
 
 
@@ -78,6 +113,7 @@ x_test, y_test = leer_datos(datos_test)
 print("Leidos ", y_test.size, " datos con sus respectivas etiquetas")
 
 print("Cada dato del conjunto de datos tiene ", x_test[0].size, " variables.")
+input("\n--- Pulsar tecla para continuar ---\n")
 
 # https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray-in-python
 unique, contador = np.unique(y_test, return_counts=True)
@@ -94,36 +130,14 @@ plt.xticks(np.arange(10))
 plt.bar(np.arange(10), contador, width=0.4)
 
 plt.show()
+input("\n--- Pulsar tecla para continuar ---\n")
+
 
 ridgeC = RidgeClassifier()
-# ajustamos el modelo
-ridgeC.fit(x,y)
-# aplicamos validación cruzada
-resultado_regresion_lineal = cross_val_score(ridgeC, x, y, scoring='neg_mean_squared_error', cv=10)
-
-
-# predecimos test acorde al modelo
-y_predecida = ridgeC.predict(x_test).round()
-
-# miramos la tasa de aciertos, es decir, cuantos ha clasificado bien
-aciertos = accuracy_score(y_test, y_predecida)
-
-print("Porcentaje de aciertos usando Ridge: ", aciertos)
-
-
+print("Evaluando Ridge Classifier:")
+evaluar(ridgeC, x, y, x_test, y_test, 10)
 
 
 SGD = SGDClassifier()
-# ajustamos el modelo
-SGD.fit(x,y)
-# aplicamos validación cruzada
-resultado_regresion_lineal = cross_val_score(SGD, x, y, scoring='neg_mean_squared_error', cv=10)
-
-
-# predecimos test acorde al modelo
-y_predecida = SGD.predict(x_test).round()
-
-# miramos la tasa de aciertos, es decir, cuantos ha clasificado bien
-aciertos = accuracy_score(y_test, y_predecida)
-
-print("Porcentaje de aciertos usando SGD: ", aciertos)
+print("Evaluando SGD Classifier:")
+evaluar(SGD, x, y, x_test, y_test, 10)
